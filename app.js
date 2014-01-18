370a2cd128fcb48ec2cd5c801497de12a8e75f84
Nikhil <nikhil@activesphere.com>
2014-01-18 18:01:30 +0530

    image grayscale

--- app.js
+++ app.js
@@ -9,22 +9,36 @@ app.use('/', express.static(__dirname + '/public'));
 
 app.get('/play', function(req, res){  
 
-  cv.readImage("./running.jpg", function(err, mat){    
-  var i, r, g, b, v;
-  var rows = mat.size()[0];
-  var cols = mat.size()[1];
-  for (i=0; i<rows; i+=1) {
-    for (j=0; j<cols; i+=1) {
-    
-    r = mat.get(i,j);
-    console.log(r)
-    }
-  }
-  mat.save('./new.jpg');
-  res.send('Hello');
-  });
+  var Canvas = require('canvas'),
+  Image = Canvas.Image
+  , canvas = new Canvas(320, 320)
+  , ctx = canvas.getContext('2d')
+  , http = require('http');
+  fs.readFile('running.jpg', function(err, squid){
+  if (err) throw err;
+  img = new Image;
+  img.src = squid;
+  ctx.drawImage(img, 0, 0, img.width, img.height);
+  canvas.toDataURL()
 
-  
+  var imgd = ctx.getImageData(0, 0, img.width, img.height);
+  var data = imgd.data;  
+  var i, r, g, b, v;
+  for (i=0; i<data.length; i+=4) {
+    r = data[i];
+    g = data[i+1];
+    b = data[i+2];
+    v = 0.2126*r + 0.7152*g + 0.0722*b;
+    data[i] = data[i+1] = data[i+2] = v;
+  }
+
+  ctx.putImageData(imgd, 0, 0);
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(''
+    + '<meta http-equiv="refresh" content="1;" />'
+    + '<img src="' + canvas.toDataURL() + '" />');
+});
+
 
 });
 
