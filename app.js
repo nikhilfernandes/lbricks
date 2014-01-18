f2db00476d1bd455bca031d74cd864d43e366a1a
Nikhil <nikhil@activesphere.com>
2014-01-18 18:38:27 +0530

    binarize

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var app = express();
 
 app.use('/', express.static(__dirname + '/public'));
 
-app.get('/play', function(req, res){  
+app.get('/grayscale', function(req, res){  
 
   var Canvas = require('canvas'),
   Image = Canvas.Image
@@ -76,62 +76,47 @@ app.get('/invert', function(req, res){
 
 });
 
-app.get('/grayscale', function(req, res){
-  var cv = require('opencv');
+app.get('/binarize', function(req, res){  
 
-var camera = new cv.VideoCapture(0);
+  var Canvas = require('canvas'),
+  Image = Canvas.Image
+  , canvas = new Canvas(320, 320)
+  , ctx = canvas.getContext('2d')
+  , http = require('http');
+  fs.readFile('test.gif', function(err, squid){
+  if (err) throw err;
+  img = new Image;
+  img.src = squid;
+  ctx.drawImage(img, 0, 0, img.width/4, img.height/4);
+  canvas.toDataURL()
 
+  var imgd = ctx.getImageData(0, 0, img.width/4, img.height/4);
+  var data = imgd.data;
+  var i, r, g, b, v;
+  for (i=0; i<data.length; i+=4) {
+    r = data[i];
+    g = data[i+1];
+    b = data[i+2];
+    v = 0.2126*r + 0.7152*g + 0.0722*b;
+        if (v > 128) {
+            v = 255;
+        } else {
+            v = 0;
+        }
+    data[i] = data[i+1] = data[i+2] = v;
+  }
+  ctx.putImageData(imgd, 0, 0);
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(''
+    + '<meta http-equiv="refresh" content="1;" />'
+    + '<img src="' + canvas.toDataURL() + '" />');
 
-setInterval(function() {
+  });
 
-        camera.read(function(err, im) {
-
-                im.save('/tmp/cam.png');
-        });
-
-}, 1000);
 });
 
 
 
-
-// app.get('/grayscale', function(req, res){
-//   var Canvas = require('canvas'),
-//   Image = Canvas.Image
-//   , canvas = new Canvas(320, 320)
-//   , ctx = canvas.getContext('2d')
-//   , http = require('http');
-//   fs.readFile('running.jpg', function(err, squid){
-//   if (err) throw err;
-//   img = new Image;
-//   img.src = squid;
-//   ctx.drawImage(img, 0, 0, img.width / 4, img.height / 4);
-//   canvas.toDataURL()
-//   var pixels = ctx.getImageData(img)
-//   var data = pixels.data;
-//   console.log(data.inspect)
-//   var i, r, g, b, v;
-//   for (i=0; i<data.length; i+=4) {
-//     r = data[i];
-//     g = data[i+1];
-//     b = data[i+2];
-//     v = 0.2126*r + 0.7152*g + 0.0722*b;
-//     data[i] = data[i+1] = data[i+2] = v;
-//     console.log(data[i])
-//   }
-//   // console.log(pixels)
-//   ctx.putImageData(
-//     pixels,
-//     0,
-//     img.height / 4    
-//     );
-//   // ctx.drawImage(pixels, 0, 0, img.width / 4, img.height / 4);
-//   res.writeHead(200, { 'Content-Type': 'text/html' });
-//   res.end(''
-//     + '<meta http-equiv="refresh" content="1;" />'
-//     + '<img src="' + canvas.toDataURL() + '" />');
-// });
-// });
 
 app.listen(3000);
 console.log('Listening on port 3000');
