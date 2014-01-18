eef50885467711421182f3d9520da17026d6b2b6
Nikhil <nikhil@activesphere.com>
2014-01-18 17:07:01 +0530

    using opencv

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express'),
 http = require('http'),
 fs = require('fs'),
 util = require('util');
+cv = require('opencv');
 var app = express();
 
 app.use('/', express.static(__dirname + '/public'));
@@ -12,16 +13,25 @@ app.get('/play', function(req, res){
   , canvas = new Canvas(200,200)
   , ctx = canvas.getContext('2d');
 
-  fs.readFile('running.jpg', function(err, squid){
-  if (err) throw err;
+  cv.readImage("running.jpg", function(mat){    
+  var i, r, g, b, v;
+  for (i=0; i<mat.length; i+=4) {
+    r = mat[i];
+    g = mat[i+1];
+    b = mat[i+2];
+    v = 0.2126*r + 0.7152*g + 0.0722*b;
+    mat[i] = mat[i+1] = mat[i+2] = v;
+  }
+  });
+
   img = new Image;
-  img.src = squid;
+  img.src = mat;
   ctx.drawImage(img, 0, 0, img.width / 4, img.height / 4);
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.end(''
     + '<meta http-equiv="refresh" content="1;" />'
     + '<img src="' + canvas.toDataURL() + '" />');
-});
+
 });
 
 app.get('/grayscale', function(req, res){
